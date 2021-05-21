3b992c68a3ea05439ab1820af7fc6bdcbc505c52
Caleb Ford <45129343+cmf3673@users.noreply.github.com>
2021-05-20 21:19:31 -0500

    Added main components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,24 @@
 import React, { Component } from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Home from "./components/home/Home";
+import Signup from "./components/signup/Signup";
+import Login from "./components/login/Login";
+import Dashboard from "./components/dashboard/Dashboard";
 
 class App extends Component {
   render() {
-    return <h1>Hi!</h1>;
+    return (
+      <div>
+        <BrowserRouter>
+          <Switch>
+            <Route path="/signup" component={Signup} />
+            <Route path="/login" component={Login} />
+            <Route path="/dashboard" component={Dashboard} />
+            <Route exact path="/" component={Home} />
+          </Switch>
+        </BrowserRouter>
+      </div>
+    );
   }
 }
 
